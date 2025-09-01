4c13e3f688634e817fa646839f69dc7da96e3871
Sarmad Khan <sarmadk773@gmail.com>
2025-09-01 20:37:05 +0500

    Commit

--- src/layout/Basiclayout.jsx
+++ src/layout/Basiclayout.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
 
-const Basiclayout = () => {
+const BasicLayout = ({ children }) => {
   return (
-    <div>Basiclayout</div>
-  )
-}
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
 
-export default Basiclayout
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default BasicLayout;
